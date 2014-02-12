86ac4d82adcb957aa135dca09373077182406bda
Jonathan Cochran <jono.cochran@gmail.com>
2014-02-11 17:30:03 -0800

    updated project page

--- update-cauldron.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-TEMP=$(mktemp -d tmp.XXXXXXXX)
-wget https://github.com/BetSmartMedia/cauldron/archive/master.zip -O ${TEMP}/cauldron-master.zip
-rm -fr scss/cauldron
-unzip ${TEMP}/cauldron-master.zip -d ${TEMP}
-cp -R ${TEMP}/cauldron-master/css/cauldron css/
-cp -R ${TEMP}/cauldron-master/scss/cauldron scss/cauldron
-cp -R ${TEMP}/cauldron-master/examples.html ./
-rm -fr ${TEMP}
